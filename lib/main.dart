67888f9ba57ef82be927e4a37a348491904e8786
Nuri Ensing <ensing89@gmail.com>
2023-07-11 00:26:48 +0200

    fixed main dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@
 ** The main application file. It runs the Flutter app and sets up the route management.
 */
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // Import provider
 import 'screens/home_page.dart';
 import 'screens/book_detail_page.dart';
+import 'states/app_state.dart'; // Import your AppState
 
 void main() {
   runApp(MyApp()); // This is where the app starts executing.
@@ -12,14 +14,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(), // HomePage is mapped to the route '/'
-        '/book': (context) =>
-            BookDetailPage(), // BookDetailPage is mapped to the route '/book'
-        // You can add more routes here as your app grows
-      },
+    return ChangeNotifierProvider<AppState>(
+      // Wrap your MaterialApp with ChangeNotifierProvider
+      create: (context) => AppState(), // Initialize your AppState
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(), // HomePage is mapped to the route '/'
+          '/book': (context) =>
+              BookDetailPage(), // BookDetailPage is mapped to the route '/book'
+          // You can add more routes here as your app grows
+        },
+      ),
     );
   }
 }
